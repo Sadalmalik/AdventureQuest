6059fe1f97282fe4ea1de3405a4074677230b098
Gleb <neurothrop@gmail.com>
2020-12-19 00:24:46 +0300

    Stuff;
    Signed-off-by: Gleb <neurothrop@gmail.com>

---
+++ Assets/Code/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 rotate;
+    
+    void Update()
+    {
+        transform.Rotate(rotate * Time.deltaTime);
+    }
+}
