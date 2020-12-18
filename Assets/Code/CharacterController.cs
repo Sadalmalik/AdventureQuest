7df3ef60845ab9a695b1c649a2cf9ab80faff30f
Gleb <neurothrop@gmail.com>
2020-12-19 00:01:34 +0300

    Everything stuff;
    Signed-off-by: Gleb <neurothrop@gmail.com>

---
+++ Assets/Code/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Camera camera;
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            MoveCommand();
+        }
+    }
+    
+    public void MoveCommand()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+}
